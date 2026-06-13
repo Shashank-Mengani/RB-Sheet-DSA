c725830de2441de935d63327e1e2a6897c76c231
shashank <shashankvarma927@gmail.com>
2026-06-13 12:23:15 +0530

    Add 3Sum solution using two pointers

--- Array/src/TwoPointer.java
+++ Array/src/TwoPointer.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class TwoPointer {
     public static void main(String[] args) {
@@ -13,6 +15,9 @@ public class TwoPointer {
         int[] arr = {2,0,2,1,1,0};
         sortColors(arr);
         System.out.println(Arrays.toString(arr));
+
+        int[] elements = {-1,0,1,2,-1,-4};
+        System.out.println(threeSum(elements));
     }
 
     // 283. Move Zeroes
@@ -73,5 +78,43 @@ public class TwoPointer {
                 right--;
             }
         }
+    }
+
+    // 15. 3Sum
+    static List<List<Integer>> threeSum(int[] nums){
+        List<List<Integer>> result = new ArrayList<>();
+        Arrays.sort(nums);
+        int n = nums.length;
+
+        for (int i = 0; i < n-2; i++) {
+            // skipping repeated values
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+
+            int left = i + 1;
+            int right = n - 1;
+            while (left < right){
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0){
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+
+                    // skip duplicate elements
+                    while (left < right && nums[left] == nums[left + 1]){
+                        left++;
+                    }
+                    while (left < right && nums[right] == nums[right - 1]){
+                        right--;
+                    }
+                    left++;
+                    right--;
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        return result;
     }
 }
